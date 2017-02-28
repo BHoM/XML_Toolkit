51f7a0acb2bba9f14f07e48f7346341a17c783fd
oscarborgstrom <Oscar.Borgstrom@burohappold.com>
2017-02-28 11:23:18 +0000

    Added object model

---
+++ XML_Adapter/gbXMLSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XML_Adapter;
+using BHoM.Base;
+using System.Xml.Serialization;
+
+
+namespace XML_Adapter
+{
+
+
+    public class gbXMLSerializer
+    {
+        public static gbXML Serialize(List<string> Input)
+        {
+            gbXML bhomGbXML = new gbXML();
+            bhomGbXML.input = Input;
+            return bhomGbXML;
+        }
+    }
+}
