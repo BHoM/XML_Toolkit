a5bc4aaf3d19e55a4d8873eb72994efe32868cdf
oscarborgstrom <Oscar.Borgstrom@burohappold.com>
2017-03-06 11:16:55 +0000

    Overall-updates
    Overall-updates

---
+++ XML_Adapter/gbXML/gbXMLDerializer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XML = XML_Adapter.gbXML;
+using BHoM.Base;
+using BHE = BHoM.Environmental.Elements;
+using BHG = BHoM.Geometry;
+using System.Xml.Serialization;
+
+
+namespace XML_Adapter.gbXML
+{
+
+
+    public class gbXMLDeserializer
+    {
+        private static BHG.Group<BHG.Curve> MakeCurveGroup(Polyloop ploop)
+        {
+            List<BHG.Point> pts = new List<BHoM.Geometry.Point>();
+            foreach (CartesianPoint Cpt in ploop.CartesianPoint)
+            {
+                BHG.Point pt = new BHG.Point();
+                pt.X = Cpt.Coordinate[0];
+                pt.Y = Cpt.Coordinate[1];
+                pt.Z = Cpt.Coordinate[2];
+                pts.Add(pt);
+            }
+            BHG.Polyline pline = new BHG.Polyline(pts);
+            BHG.Group<BHG.Curve> crvs = new BHG.Group<BHoM.Geometry.Curve>();
+            crvs.Add(pline);
+            return crvs;
+        }
+        private static BHG.Polyline MakePolyline(Polyloop ploop)
+        {
+            List<BHG.Point> pts = new List<BHoM.Geometry.Point>();
+            foreach (CartesianPoint Cpt in ploop.CartesianPoint)
+            {
+                BHG.Point pt = new BHG.Point();
+                pt.X = Cpt.Coordinate[0];
+                pt.Y = Cpt.Coordinate[1];
+                pt.Z = Cpt.Coordinate[2];
+                pts.Add(pt);
+            }
+            BHG.Polyline pline = new BHG.Polyline(pts);
+            return pline;
+        }
+        public static List<BHoMObject> Deserialize(gbXML gbx)
+        { 
+            List<BHoMObject> bhomObjects = new List<BHoMObject>();
+            List<XML.Surface> srfs = gbx.Campus.Surface;
+            List<XML.Space> spaces = gbx.Campus.Building[0].Space;
+            // Generate gbXMLSurfaces
+            if (srfs !=null)
+            {
+                foreach (Surface srf in srfs)
+                {
+
+                    BHE.Panel pan = new BHE.Panel();
+                    pan.Name = srf.Name;
+                    pan.Type = srf.surfaceType;
+                    pan.CustomData.Add("gbXML-ID", srf.id);
+                    pan.External_Contours = MakeCurveGroup(srf.PlanarGeometry.PolyLoop);
+                    bhomObjects.Add(pan);
+                }
+            }
+
+        // Generate gbXMLSpaces
+            if (spaces != null)
+            {
+                foreach (XML.Space space in spaces)
+                {
+                    BHE.Space xspace = new BHE.Space();
+                    xspace.Name = space.Name;
+                    xspace.CustomData.Add("gbXML-ID", space.id);
+                    foreach (XML.Polyloop ploop in space.ShellGeometry.ClosedShell.PolyLoop)
+                    {
+                        xspace.Polylines.Add(MakePolyline(ploop));
+                    }
+                    bhomObjects.Add(xspace);
+                }
+            }
+            return bhomObjects;
+        }
+    }
+}
