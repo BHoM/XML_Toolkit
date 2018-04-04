100845f5af835dc6e635ba5de9abc60facabe57c
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2018-04-04 19:29:40 +0100

    deleted unused method

--- XML_Engine/Query/Length.cs
+++ XML_Engine/Query/Length.cs
@@ -1,33 +1,42 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using BHG = BH.oM.Geometry;
-//using BH.Engine.Geometry;
-//using System.Linq;
-//using BH.oM.Environmental.Elements;
+﻿using System;
+using System.Collections.Generic;
+using BHG = BH.oM.Geometry;
+using BH.Engine.Geometry;
+using System.Linq;
+using BH.oM.Environmental.Elements;
 
 
-//namespace BH.Engine.XML
-//{
+namespace BH.Engine.XML
+{
 
-//    public static partial class Query
-//    {
-//        /***************************************************/
-//        /**** Public Methods                            ****/
-//        /***************************************************/
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
 
-//        public static double Length(BHG.Polyline pline)
-//        {
-//            double length = 0;
-//            List<BHG.Point> pts = BoundingCurve(pline).DiscontinuityPoints();
-//            if (pts.Count == 4)
-//                    length = Geometry.Query.Distance(pts[0], pts[1]);
-            
-//            return length;
-//        }
+        public static double Length(BHG.Polyline pline)
+        {
 
-//        /***************************************************/
-//    }
-//}
+            List<BHG.Point> pts = pline.DiscontinuityPoints();
+
+            double length = pts.Last().Distance(pts.First());
+
+            for (int i = 0; i < pts.Count - 1; i++)
+            {
+                double dist = pts[i].Distance(pts[i + 1]);
+                length = dist > length ? dist : length;
+            }
+
+            double area = pline.Area();
+            double width = area / length;
+
+            return length;
+        }
+
+        /***************************************************/
+    }
+}
 
 
 
