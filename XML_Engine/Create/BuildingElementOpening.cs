446916e04bddba344f80306faed604897f56ac44
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2018-06-01 11:58:46 +0100

    tidied up the code

--- XML_Engine/Create/BuildingElementOpening.cs
+++ XML_Engine/Create/BuildingElementOpening.cs
@@ -12,6 +12,7 @@ using BH.Engine.Environment;
 
 namespace BH.Engine.XML
 {
+    //TODO: move these methods to the Environment Engine
 
     public static partial class Create
     {
@@ -29,80 +30,27 @@ namespace BH.Engine.XML
 
         /***************************************************/
 
-        public static BHE.BuildingElement BuildingElementOpening(this BHE.BuildingElement be, List<BHG.PolyCurve> polyCurve)
-        {
-
-            string revitElementID = (be.BuildingElementProperties.CustomData["Revit_elementId"]).ToString();
-
-            foreach (BHG.PolyCurve pCrv in polyCurve)
-            {
-                BHE.BuildingElementOpening opening = BuildingElementOpening(pCrv);
-
-                //Use the same properties as the wall
-                opening.Name = be.Name;
-                opening.CustomData.Add("Revit_elementId", revitElementID);
-
-                (be.BuildingElementGeometry as BHE.BuildingElementPanel).Openings.Add(opening);
-            }
-
-            return be;
-        }
-
-        /***************************************************/
-
-        public static BHE.BuildingElement BuildingElementOpening(this BHE.BuildingElement be, BHG.Polyline pline)
-        {
-
-            string revitElementID = (be.BuildingElementProperties.CustomData["Revit_elementId"]).ToString();
-            BHG.PolyCurve pCrv = be.BuildingElementGeometry.ICurve() as BHG.PolyCurve;
-
-            BHE.BuildingElementOpening opening = BuildingElementOpening(pCrv);
-
-            //Use the same properties as the wall
-            opening.Name = be.Name;
-            opening.CustomData.Add("Revit_elementId", revitElementID);
-
-            (be.BuildingElementGeometry as BHE.BuildingElementPanel).Openings.Add(opening);
-
-            return be;
-        }
-
-        /***************************************************/
-
         public static BHE.BuildingElement BuildingElementOpening(this BHE.BuildingElement be, BHG.ICurve bound)
         {
+            if (be == null)
+                return be;
 
             string revitElementID = (be.BuildingElementProperties.CustomData["Revit_elementId"]).ToString();
             BHG.PolyCurve pCrv = bound as BHG.PolyCurve;
-
             BHE.BuildingElementOpening opening = BuildingElementOpening(pCrv);
 
-            //Use the same properties as the wall
-            opening.Name = be.Name;
-            opening.CustomData.Add("Revit_elementId", revitElementID);
-
-            (be.BuildingElementGeometry as BHE.BuildingElementPanel).Openings.Add(opening);
-
-            return be;
-        }
-
-        /***************************************************/
-
-        public static BHE.BuildingElementPanel BuildingElementOpening(this BHE.BuildingElementPanel panel, BHE.BuildingElement be, BHG.ICurve bound)
-        {
-
-            string revitElementID = (be.BuildingElementProperties.CustomData["Revit_elementId"]).ToString();
-            BHG.PolyCurve pCrv = bound as BHG.PolyCurve;
-
-            BHE.BuildingElementOpening opening = BuildingElementOpening(pCrv);
+            BHE.BuildingElementPanel panel = be.BuildingElementGeometry as BHE.BuildingElementPanel;
+            if (panel == null) // if be isn't of type buildingElementPanel
+                return be;
 
             //Use the same properties as the wall
             opening.Name = be.Name;
             opening.CustomData.Add("Revit_elementId", revitElementID);
 
             panel.Openings.Add(opening);
+            be.BuildingElementGeometry = panel;
 
-            return panel;
+            return be;
         }
 
         /***************************************************/
