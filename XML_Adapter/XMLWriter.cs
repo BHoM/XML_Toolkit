a829f413bdb48b8045d707dde7c0632638054d6a
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-05 12:32:14 +0100

    Bug fix in file export

--- XML_Adapter/XMLWriter.cs
+++ XML_Adapter/XMLWriter.cs
@@ -7,6 +7,8 @@ using System.Xml.Serialization;
 using System.Xml;
 using System.IO;
 
+using BH.oM.Base;
+
 namespace XML_Adapter
 {
     public class XMLWriter
@@ -16,9 +18,15 @@ namespace XML_Adapter
         {
             try
             {
+                System.Reflection.PropertyInfo[] bhomProperties = typeof(BHoMObject).GetProperties();
+                XmlAttributeOverrides overrides = new XmlAttributeOverrides();
+
+                foreach(System.Reflection.PropertyInfo pi in bhomProperties)
+                    overrides.Add(typeof(BHoMObject), pi.Name, new XmlAttributes { XmlIgnore = true });
+
                 Name += ".xml";
                 XmlSerializerNamespaces xns = new XmlSerializerNamespaces();
-                XmlSerializer szer = new XmlSerializer(typeof(BH.oM.XML.GBXML));
+                XmlSerializer szer = new XmlSerializer(typeof(BH.oM.XML.GBXML), overrides);
                 TextWriter ms = new StreamWriter(Path.Combine(filePath, Name));
                 szer.Serialize(ms, gbx, xns);
                 ms.Close();
