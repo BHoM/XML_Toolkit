8d61b644e40b1551575a1a180969aab4a126f6e8
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-18 14:18:50 +0100

    Completing Issue 83 - folder structure for testing XML Toolkit

---
+++ XML_Test/Adapter/CRUD/Create.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BH.Adapter.XML;
+using BH.Engine.XML;
+
+using BH.oM.Environment.Elements;
+
+using System.Collections.Generic;
+
+namespace BH.Test.XML
+{
+    [TestClass]
+    public class Create
+    {
+        [TestMethod]
+        public void TestCreate_1()
+        {
+            //Test the adapter Create functions
+        }
+    }
+}
