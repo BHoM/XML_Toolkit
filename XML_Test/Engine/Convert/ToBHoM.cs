c772cdaf86300267cf865990e02e372f99945f44
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-18 14:18:50 +0100

    Completing Issue 83 - folder structure for testing XML Toolkit

---
+++ XML_Test/Engine/Convert/ToBHoM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BH.Adapter.XML;
+using BH.Engine.XML;
+
+using BH.oM.Environment.Elements;
+
+using System.Collections.Generic;
+
+namespace BH.Test.XML
+{
+    [TestClass]
+    public class ToBHoM
+    {
+        [TestMethod]
+        public void TestToBHoM_1()
+        {
+            //Test the conversion of point to BHoM Point
+        }
+    }
+}
---
+++ XML_Test/Engine/Modify/CADObjectID.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BH.Adapter.XML;
+using BH.Engine.XML;
+
+using BH.oM.Environment.Elements;
+
+using System.Collections.Generic;
+
+namespace BH.Test.XML
+{
+    [TestClass]
+    public class CADObjectID
+    {
+        [TestMethod]
+        public void TestCADObjectID_1()
+        {
+            //Test changing of CAD Object IDs
+        }
+    }
+}
