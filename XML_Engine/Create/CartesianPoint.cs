79efd29a87f0022c87fee48dbbd6772f7ce5f99e
Maria Tjader <maria.tjader@burohappold.com>
2018-09-21 12:11:09 +0100

    Function for Create point

---
+++ XML_Engine/Create/CartesianPoint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.XML;
+
+namespace BH.Engine.XML
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static CartesianPoint CartesianPoint(string x = "", string y = "", string z = "")
+        { 
+            CartesianPoint pt = new CartesianPoint();
+            string[] coords = new string[3];
+            coords[0] = x;
+            coords[1] = y;
+            coords[2] = z;
+            pt.Coordinate = coords;
+            return pt;
+        }
+
+        public static CartesianPoint CartesianPoint(int x = 0, int y = 0, int z = 0)
+        {
+            return CartesianPoint(x.ToString(), y.ToString(), z.ToString());
+        }
+
+        public static CartesianPoint CartesianPoint(double x = 0, double y = 0, double z= 0)
+        {
+            return CartesianPoint(x.ToString(), y.ToString(), z.ToString());   
+        }
+    }
+}
