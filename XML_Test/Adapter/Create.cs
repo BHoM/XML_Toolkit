44f01d02ae17935bddd19d9bcd0dc6ac7b62760e
Maria Tjader <maria.tjader@burohappold.com>
2018-09-25 11:53:00 +0100

    Changing names in ToGBXML-Surface-Test

---
+++ XML_Test/Adapter/Create.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XML_Test.Adapter
+{
+    [TestClass]
+    public class Create
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
