c010fe14f0938dbb6c950b1f47c5a6d51c084e23
rolyh <roly.hudson@gmail.com>
2020-04-30 11:47:44 +0100

    Set up Adapter for KML

---
+++ XML_Adapter/Objects/KML/Enum/ColourMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BH.oM.External.XML.KMLSchema
+{
+    public enum ColourMode
+    {
+        [XmlEnum(Name = "normal")]
+        Normal,
+        [XmlEnum(Name = "random")]
+        Random
+    }
+}
