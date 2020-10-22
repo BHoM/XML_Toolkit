db430ccf308d4d6c79c1da416fb5cd7984c58581
Johanna Olin <Johanna.Olin@BuroHappold.com>
2020-10-22 12:28:02 +0100

    Added new fragment XMLBUildingType and replaced CustomData in Building file

---
+++ XML_oM/Fragments/XMLBuildingType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+
+namespace BH.oM.XML.Fragments
+{
+    public class XMLBuildingType : IFragment
+    {
+        public virtual string Type { get; set; } = "";
+    }
+}
