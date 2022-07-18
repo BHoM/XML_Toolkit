94a53a3e711a5976a5fb3444c61bd60bad71492f
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:04 +0100

    Update Assembly Information

--- XML_Engine/Properties/AssemblyInfo.cs
+++ XML_Engine/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("XML_Engine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/XML_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("XML_Engine")]
