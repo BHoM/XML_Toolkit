b672bd9b4bc6cd0b1f1b68aae97df5724b08272a
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2023-10-30 10:22:30 +0000

    added test for units, and added copyright string

--- .ci/unit-tests/XML_Adapter_Tests/PushTests.cs
+++ .ci/unit-tests/XML_Adapter_Tests/PushTests.cs
@@ -1,4 +1,26 @@
-﻿using NUnit.Framework;
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using NUnit.Framework;
 using FluentAssertions;
 using System;
 using System.Collections.Generic;
@@ -37,6 +59,7 @@ namespace BH.Tests.Adapter.XML
             m_config = new XMLConfig()
             {
                 File = new FileSettings() { Directory = ModelsPath, FileName = "PushedModel.xml"},
+                Schema = oM.Adapters.XML.Enums.Schema.GBXML
             };
             m_jsonObjects = BH.Engine.Adapters.File.Compute.ReadFromJsonFile(Path.Combine(ModelsPath, "TestModel.json"), true).Cast<IBHoMObject>().ToList();
             m_jsonPanels = BH.Engine.Environment.Query.Panels(m_jsonObjects);
@@ -67,11 +90,17 @@ namespace BH.Tests.Adapter.XML
         [Description("Test for pushing a set of BHoM environment objects to xml with constructions set to true.")]
         public void PushGBXML()
         {
-            m_config.Schema = oM.Adapters.XML.Enums.Schema.GBXML;
             m_config.Settings = new GBXMLSettings() 
             {
                 IncludeConstructions = true,
-                ExportDetail = oM.Adapters.XML.Enums.ExportDetail.Full
+                ExportDetail = oM.Adapters.XML.Enums.ExportDetail.Full,
+                UnitSetUp = new GBXMLUnitSetUp()
+                {
+                    TemperatureUnit = TemperatureUnit.Celcius,
+                    LengthUnit = LengthUnit.Meters,
+                    AreaUnit = AreaUnit.SquareMeters,
+                    VolumeUnit = VolumeUnit.CubicMeters
+                }
             };
             FilterRequest request = new FilterRequest();
 
@@ -102,11 +131,17 @@ namespace BH.Tests.Adapter.XML
         [Description("Test for pushing a set of BHoM environment objects to xml with constructions set to false.")]
         public void PushGBXMLNoConstructions()
         {
-            m_config.Schema = oM.Adapters.XML.Enums.Schema.GBXML;
             m_config.Settings = new GBXMLSettings() 
             { 
                 IncludeConstructions = false,
-                ExportDetail = oM.Adapters.XML.Enums.ExportDetail.Full
+                ExportDetail = oM.Adapters.XML.Enums.ExportDetail.Full,
+                UnitSetUp = new GBXMLUnitSetUp()
+                {
+                    TemperatureUnit = TemperatureUnit.Celcius,
+                    LengthUnit = LengthUnit.Meters,
+                    AreaUnit = AreaUnit.SquareMeters,
+                    VolumeUnit = VolumeUnit.CubicMeters
+                }
             };
             FilterRequest request = new FilterRequest();
 
@@ -136,11 +171,17 @@ namespace BH.Tests.Adapter.XML
         [Description("Test for pushing GBXML with exportDetail set to BuildingShell")]
         public void PushGBXMLBuildingShell()
         {
-            m_config.Schema = oM.Adapters.XML.Enums.Schema.GBXML;
             m_config.Settings = new GBXMLSettings()
             {
                 IncludeConstructions = false,
-                ExportDetail = oM.Adapters.XML.Enums.ExportDetail.BuildingShell
+                ExportDetail = oM.Adapters.XML.Enums.ExportDetail.BuildingShell,
+                UnitSetUp = new GBXMLUnitSetUp()
+                {
+                    TemperatureUnit = TemperatureUnit.Celcius,
+                    LengthUnit = LengthUnit.Meters,
+                    AreaUnit = AreaUnit.SquareMeters,
+                    VolumeUnit = VolumeUnit.CubicMeters
+                }
             };
             FilterRequest request = new FilterRequest();
 
@@ -164,6 +205,33 @@ namespace BH.Tests.Adapter.XML
             constructions.Count.Should().Be(0, "No constructions should be pulled, but some were pulled anyway.");
 
             objs.Count.Should().Be(42, "There was a different number of objects pulled compared to expected.");
+        }
+
+        [Test]
+        [Description("Test for pushing GBXML with different units set.")]
+        public void PushGBXMLUnits()
+        {
+            m_config.Settings = new GBXMLSettings()
+            {
+                IncludeConstructions = false,
+                ExportDetail = oM.Adapters.XML.Enums.ExportDetail.Full,
+                UnitSetUp = new GBXMLUnitSetUp()
+                {
+                    TemperatureUnit = TemperatureUnit.Kelvin,
+                    LengthUnit = LengthUnit.Feet,
+                    AreaUnit = AreaUnit.SquareFeet,
+                    VolumeUnit = VolumeUnit.CubicFeet
+                }
+            };
+            
+            m_adapter.Push(m_jsonObjects, actionConfig: m_config);
+
+            List<string> lines = File.ReadAllLines(m_config.File.GetFullFileName()).ToList();
+
+            lines[1].Should().Contain("lengthUnit=\"Feet\"", "The length unit was not set to Feet.");
+            lines[1].Should().Contain("areaUnit=\"SquareFeet\"", "The area unit was not set to Square Feet.");
+            lines[1].Should().Contain("volumeUnit=\"CubicFeet\"", "The volume unit was not set to Cubic Feet.");
+            lines[1].Should().Contain("temperatureUnit=\"K\"", "The temperature unit was not set to Kelvin.");
         }
     }
 }
